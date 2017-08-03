242d9d61351008e9bae5571e8cde49698383302f
Fabrizio Bertone <fab.bertone@gmail.com>
2017-08-03 20:37:44 +0200

    fix documentation identation

--- media.py
+++ media.py
@@ -1,9 +1,9 @@
 # class for Movies
 
 class Movie():
-""" The Movie class creates object of type Movie
-Three string arguments must be passed: title, poster image url and youtube trailer url
-"""  
+    """ The Movie class creates object of type Movie
+    Three string arguments must be passed: title, poster image url and youtube trailer url
+    """  
     def __init__(self, title, poster, trailer):
         self.title = title
         self.poster_image_url = poster
