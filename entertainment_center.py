5b4318d87f99fbf6bae516a5aa6058b677e03de5
Fabrizio Bertone <fab.bertone@gmail.com>
2017-08-04 22:15:58 +0200

    fix prep8

--- entertainment_center.py
+++ entertainment_center.py
@@ -3,13 +3,16 @@ import media
 import fresh_tomatoes
 
 # add or create new entries following the examples
-# each Movie object must be initialized with 3 strings: the title, the poster and the link to the trailer on youtube
-machete = media.Movie("Machete",  
-    "https://images-na.ssl-images-amazon.com/images/M/MV5BZWUxODc2NmItNThkNS00Mzc4LThlYTQtOTYwZjVhYjRiNmMwXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg", 
+# each Movie object must be initialized with 3 strings:
+# the title, the poster and the link to the trailer on youtube
+machete = media.Movie(
+    "Machete",
+    "https://images-na.ssl-images-amazon.com/images/M/MV5BZWUxODc2NmItNThkNS00Mzc4LThlYTQtOTYwZjVhYjRiNmMwXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg",  # noqa
     "https://www.youtube.com/watch?v=I16020r--oM")
 
-machete_kills = media.Movie("Machete Kills",
-    "https://images-na.ssl-images-amazon.com/images/M/MV5BMjA2MzUxMTM3M15BMl5BanBnXkFtZTgwMzA2NzkxMDE@._V1_SY1000_CR0,0,674,1000_AL_.jpg",
+machete_kills = media.Movie(
+    "Machete Kills",
+    "https://images-na.ssl-images-amazon.com/images/M/MV5BMjA2MzUxMTM3M15BMl5BanBnXkFtZTgwMzA2NzkxMDE@._V1_SY1000_CR0,0,674,1000_AL_.jpg",  # noqa
     "https://www.youtube.com/watch?v=DwbG64YC-vQ")
 
 # add all the Movies in this array
